c918fa7787a129c4bf271904c384054811e77d0a
Dmirty Kantin <Dmitry_Kantin@epam.com>
2017-09-25 20:45:01 +0300

    Feature/home task 2 (#1)
    * add: use nodemon
    
    * add: implement dirwatcher module
    
    * add: implement importer module

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import * as Config from './config';
 import { User, Product } from './models';
+import dirWatcherEventEmitter from 'modules/dirwatcher/eventEmitter';
+import { DirwatcherEvents } from 'modules/dirwatcher/constants';
+import DirWatcher from 'modules/dirwatcher';
+import Importer from 'modules/importer';
 
-console.log(Config.name);
 const user = new User();
 const product = new Product();
+const dirWatcher = new DirWatcher();
+const importer = new Importer();
+
+console.log(Config.name);
+dirWatcher.watch(Config.dataPath, 2000);
+importer.subscribeToEvent(dirWatcherEventEmitter, DirwatcherEvents.CHANGE, { isSync: false });
