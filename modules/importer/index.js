8aef1b2d240d690577bbc3414241e9085086ea10
Dmitry Kantin <kantindm@gmail.com>
2017-09-25 20:41:30 +0300

    add: implement importer module

---
+++ modules/importer/index.js
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import { dataPath } from 'config';
+import parseCSVSSync from 'csv-parse/lib/sync';
+import parse from 'csv-parse';
+
+export default class Importer {
+
+    subscribeToEvent(emitter, eventName, options = { isSync: false }) {
+        if (options.isSync) {
+            emitter.on(eventName, this.importSync.bind(this));
+        } else {
+            emitter.on(eventName, this.import.bind(this));
+        }
+    }
+
+    import(fileName) {
+        return new Promise((resolve, reject) => {
+            const importedData = [];
+            const fullPath = path.join(dataPath, fileName);
+            const parser = parse({ delimiter: ',', columns: true });
+            let record;
+
+            parser.on('error', reject);
+            parser.on('finish', () => resolve(importedData));
+            parser.on('readable', () => {
+                while (record = parser.read()) {
+                    importedData.push(record);
+                }
+            });
+
+            fs.createReadStream(fullPath).pipe(parser);
+        });
+    }
+
+    importSync(fileName) {
+        const fullPath = path.join(dataPath, fileName);
+        const fileContent = fs.readFileSync(fullPath, { encoding: 'utf8' });
+        const importedData = parseCSVSSync(fileContent, { columns: true });
+
+        return importedData;
+    }
+}
